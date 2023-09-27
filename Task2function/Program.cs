28aca6e0e73726ec7d52c6eb62cae253c6d85bc9
dmitriev <nafoko@mail.ru>
2023-09-27 15:31:13 +0300

    выполнено task2function

--- Task2function/Program.cs
+++ Task2function/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = new Random().Next(100, 1000);
+int deleteSecondDigit = DeleteSecondDigit(number);
+Console.WriteLine($"{number} -> {deleteSecondDigit}");
+int DeleteSecondDigit(int num)
+{
+    int firstDigit = number / 100;
+    int lastDigit = number % 10;
+    int numResult = firstDigit * 10 + lastDigit;
+    return numResult;
+}
