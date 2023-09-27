6dd8f321e336140a953ae64918b16e69020e2171
dmitriev <nafoko@mail.ru>
2023-09-27 14:47:27 +0300

    Task1function

---
+++ Task1function/Program.cs
@@ -0,0 +1,13 @@
+﻿// Решение задачи с промощью функции
+int number = new Random().Next(10, 99);
+Console.WriteLine($"Случайное число из отрезка [10, 99] -> {number}");
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
