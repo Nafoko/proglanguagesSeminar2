f726af70b0a2ce7ac0b7dcfd013fe9336eb6c9b5
dmitriev <nafoko@mail.ru>
2023-09-28 14:19:19 +0300

    task6omashka

---
+++ Task6Domashka/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа, которая принимает на вход
+// трехзначное число и на выходе покахывает вторую цифру этого числа.
+
+
+Console.WriteLine ($"Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int secondDigit = SecondDigit(number);
+Console.WriteLine($"{number} -> {secondDigit}");
+
+int SecondDigit(int num)
+{
+    int firstDigit = number / 100;
+    int lastDigit = number % 10;
+    int numResult = number / 10 - lastDigit;
+    return numResult;
+}
+
+
