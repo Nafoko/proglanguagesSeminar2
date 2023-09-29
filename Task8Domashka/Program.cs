5eb16c715bdfacd327c33fe43b5f4bad32d7ec74
dmitriev <nafoko@mail.ru>
2023-09-29 12:14:35 +0300

    task8domashka

---
+++ Task8Domashka/Program.cs
@@ -0,0 +1,52 @@
+﻿// // Задача 15: Напишите программу, 
+// которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет,
+//  является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Internal;
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+bool IsWeekend(int weekDay)
+{
+    if(weekDay > 5)
+    {
+    return true;
+    }
+return false;
+}
+
+
+bool ValidateWeekDay(int number)
+{
+    if (number > 0 && number <= 7)
+    {
+        return true;
+    }
+    Console.WriteLine("Это не день недели!");
+    return false;
+}
+
+int weekDay = Promt("Введите день недели >");
+if (ValidateWeekDay(weekDay))
+{
+    if (IsWeekend(weekDay))
+    {
+        Console.WriteLine("Наконец-то выходной");
+    }
+    else
+    {
+        Console.WriteLine("Придется поработать");
+    }
+}
